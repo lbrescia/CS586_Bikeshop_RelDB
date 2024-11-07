00d8eb9f4c61c4be3dd66df1c0c1192940827fc0
Logan Brescia <lbrescia@pdx.edu>
2024-11-06 22:14:10 -0800

    Need to find a better solution for using the load_csv, need to set env variable scriptdir to the current path

---
+++ load_csv.sql
@@ -0,0 +1,9 @@
+-- Load all of the csv files into the database
+\cd :scriptdir
+
+\set csvfile :scriptdir'/csv/customers.csv'
+
+COPY customers(customer_id, first_name, last_name, phone, email, street, city, "state", zip_code)
+FROM :'csvfile'
+DELIMITER ','
+CSV HEADER;
--- schema.sql
+++ schema.sql
@@ -3,8 +3,11 @@ CREATE TABLE customers (
 	customer_id  SERIAL PRIMARY KEY,
 	first_name	 VARCHAR(50),
 	last_name 	 VARCHAR(50),
-	phone		 CHAR(11),
+	phone		 VARCHAR(15),
 	email		 VARCHAR(50),
 	street		 VARCHAR(100),
 	city		 VARCHAR(100),
+	"state"		 VARCHAR(14),
+	zip_code	 VARCHAR(10)
+);
 
