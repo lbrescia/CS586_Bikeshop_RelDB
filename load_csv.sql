520a66f2660930ec5cbfa331d65781bcda68d818
Logan Brescia <lbrescia@pdx.edu>
2024-11-11 12:33:49 -0800

    Updated the schema and load file to include the orders, order_item, and staffs table, need to finish the copy function for staffs

--- load_csv.sql
+++ load_csv.sql
@@ -1,10 +1,27 @@
+-- If there are any tables in the database, delete them and reset the sequence
+TRUNCATE TABLE order_items, orders, customers RESTART IDENTITY CASCADE;
+
 -- Load all of the csv files into the database
 \cd :scriptdir
 
 \set csvfile :scriptdir'/csv/customers.csv'
-
 COPY customers(customer_id, first_name, last_name, phone, email, street, city, "state", zip_code)
 FROM :'csvfile'
 DELIMITER ','
 CSV HEADER
 NULL AS 'NULL';
+
+\set csvfile :scriptdir'/csv/orders.csv'
+COPY orders(order_id, customer_id, order_status, order_date, required_date, shipped_date, store_id, staff_id)
+FROM :'csvfile'
+DELIMITER ','
+CSV HEADER
+NULL AS 'NULL';
+
+\set csvfile :scriptdir'/csv/order_items.csv'
+COPY order_items(order_id, item_id, product_id, quantity, list_price, discount)
+FROM :'csvfile'
+DELIMITER ','
+CSV HEADER
+NULL AS 'NULL';
+
--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 -- customers table domain
-CREATE TABLE customers (
+CREATE TABLE IF NOT EXISTS customers (
 	customer_id  SERIAL PRIMARY KEY,
 	first_name	 VARCHAR(50),
 	last_name 	 VARCHAR(50),
@@ -11,3 +11,43 @@ CREATE TABLE customers (
 	zip_code	 VARCHAR(10)
 );
 
+-- orders table domain
+CREATE TABLE IF NOT EXISTS orders (
+	order_id	  SERIAL PRIMARY KEY,
+	customer_id   INT,
+	order_status  VARCHAR(50),
+	order_date	  DATE,
+	required_date DATE,
+	shipped_date  DATE,
+	store_id	  INT,
+	staff_id	  INT,
+	FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+-- Explaining the order_status column values and their meanings
+COMMENT ON COLUMN orders.order_status IS '1: Pending, 2: Processing, 3: Rejected, 4: Completed';
+
+CREATE TABLE IF NOT EXISTS order_items (
+	order_id     INT,
+	item_id 	 INT,
+	product_id   INT,
+	quantity     INT,
+	list_price   MONEY,
+	discount	 DECIMAL(3, 2),      -- 2 digits, 2 decimal places
+	PRIMARY KEY (order_id, item_id), -- Composite primary key
+	FOREIGN KEY (order_id) REFERENCES orders(order_id) 
+	--FOREIGN KEY (product_id) REFERENCES products(product_id)  UNCOMMENT THIS LINE AFTER CREATING THE PRODUCTS TABLE
+);
+
+CREATE TABLE IF NOT EXISTS staffs (
+	staff_id	 SERIAL PRIMARY KEY,
+	first_name	 VARCHAR(50),
+	last_name	 VARCHAR(50),
+	email		 VARCHAR(50),
+	phone		 VARCHAR(15),
+	active		 BOOLEAN,
+	store_id	 INT,
+	manager_id	 INT,
+	FOREIGN KEY (manager_id) REFERENCES staffs(staff_id) -- Self-referencing foreign key
+	--FOREIGN KEY (store_id) REFERENCES stores(store_id)  UNCOMMENT THIS LINE AFTER CREATING THE STORES TABLE
+);
